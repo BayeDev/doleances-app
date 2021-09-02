61cff3cebb4e010734eaf0e0448b8366efc9cb75
SS <sowsoule@gmail.com>
2021-09-02 18:07:26 +0300

    tailwind set

---
+++ frontend-app/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    //extend: {},
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#22D3EE',
+      'secondary': '#ffed4a',
+      'danger': '#e3342f',
+     })
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
