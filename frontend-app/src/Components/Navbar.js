4a9c7aaa455575152a675aeca8a9a99de29a1e54
SS <sowsoule@gmail.com>
2021-09-10 13:55:18 +0300

    check merge

--- frontend-app/src/Components/Navbar.js
+++ frontend-app/src/Components/Navbar.js
@@ -19,7 +19,7 @@ import {
 export default function Navbar(){
     return (
         <div>
-            <nav className="text-grey-900 flex justify-between mx-4 p-1" style={{backgroundImage:'linear-gradient(to right, rgb(91, 121, 162) 0%, rgb(46, 68, 105) 100%)'}}>
+            <nav className="px-4  mt-0 fixed w-full z-10 top-0 text-grey-900  flex justify-between " style={{backgroundImage:'linear-gradient(to right, rgb(91, 121, 162) 0%, rgb(46, 68, 105) 100%)'}}>
                 <div className="flex justify-between space-x-4 m-2">
                     <div><Link to="/"><Logo/></Link></div>
                     <div className=""><Search/></div>
